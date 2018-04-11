09d5804877370396c8c29890b878661374f5408b
VolkMilit <javirrdar@gmail.com>
2018-04-11 11:30:26 +0800

    change license, that was been choosen by accidentally

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,3 +1,21 @@
+/*
+    CuteMail - simple mail client for GNU/Linux.
+    Copyright (C) 2018 Volk_Milit (aka Ja'Virr-Dar)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
@@ -12,6 +30,8 @@ MainWindow::MainWindow(QWidget *parent) :
     qtreeviewhelper(new QTreeViewHelper())
 {
     ui->setupUi(this);
+
+    ui->fr_warning->hide();
 
     populateTreeView();
     setupWebView();
@@ -97,6 +117,9 @@ void MainWindow::readSettings()
 
     for (int i = 0; i < column_list.length(); i++)
         ui->tb_mails->setColumnWidth(i, column_list.at(i).toInt());
+
+    QModelIndex idx = 0;
+    ui->treeView->setCurrentIndex(idx);
 }
 
 void MainWindow::writeSettings()
